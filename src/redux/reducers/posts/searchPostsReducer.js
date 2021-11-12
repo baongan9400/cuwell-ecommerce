02fe23f3d01dd564d97111396d794201afff1dd9
baongan9400 <dangbaongan9400@gmail.com>
2021-11-12 22:25:32 +0700

    change size loading card

--- src/redux/reducers/posts/searchPostsReducer.js
+++ src/redux/reducers/posts/searchPostsReducer.js
@@ -1,32 +1,32 @@
-import * as type from '../../constants'
+import * as type from "../../constants";
 
 const initialState = {
-    params: {
-        search: '',
-        category: '',
-        page: 1,
-        page_size: 18,
-    },
-    load: true,
-    data: {}
+  params: {
+    search: "",
+    category: "",
+    page: 1,
+    page_size: 12,
+  },
+  load: true,
+  data: {},
 };
 export const searchPostsReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case type.LOAD_POST:
-            return {
-                ...state,
-                params : action.params,
-                load: true,
-            };
+  switch (action.type) {
+    case type.LOAD_POST:
+      return {
+        ...state,
+        params: action.params,
+        load: true,
+      };
 
-        case type.LOADED_POST:
-            const { data } = action.data;
-            return {
-                ...state,
-                load: false,
-                data: data,
-            };
-        default:
-            return state;
-    }
-}
+    case type.LOADED_POST:
+      //   const { count, next, previous, results } = action.data;
+      return {
+        ...state,
+        load: false,
+        data: action.data,
+      };
+    default:
+      return state;
+  }
+};
